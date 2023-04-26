f2abfc4560d8c9e5d5adb1b218aee432c5d09bcb
Aim4Progress <progressisbetterthanperfection@gmail.com>
2023-04-26 12:52:34 +0530

    Created client folder and moved everything here

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
 import "./navBar.scss";
 
 const NavBar = () => {
   return (
     <div className="nav-bar">
       <div className="container">
-        <div className="logo">FreeL</div>
+        <div className="logo">
+          {/* <Link to="/"> */}
+          FreeL
+          {/* </Link> */}
+        </div>
         <div className="links">
           <span>Find Client</span>
           <span>Find Freelancer</span>
